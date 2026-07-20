ed0e0030508c93a5fc87aa6c82d4077542b8fb92
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2026-07-19 23:35:25 -0400

    test

---
+++ hypr/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "DP-1",
+    mode     = "1920x1080@180.06Hz",
+    position = "auto",
+    scale    = "auto",
+})
